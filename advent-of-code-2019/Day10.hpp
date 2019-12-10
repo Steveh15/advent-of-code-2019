118f554273c08bd0bb350990055094ba71eeed21
smackie <stephen.c.mackie@gmail.com>
2019-12-10 18:50:51 +0000

    Minor tidy up

--- advent-of-code-2019/Day10.hpp
+++ advent-of-code-2019/Day10.hpp
@@ -1,13 +1,10 @@
 #pragma once
-#include <math.h>
 #include <set>
 #include <map>
 #include <algorithm>
 #include <iostream>
 #include <vector>
 #include <corecrt_math_defines.h>
-
-
 
 
 namespace Day10 {
@@ -57,10 +54,10 @@ namespace Day10 {
 
 					for (int y2 = 0; y2 < dim; y2++) {
 						for (int x2 = 0; x2 < dim; x2++) {
-							if (input[y2][x2] == '#' and !(y2 == y and x2 == x)) {
-								//Find angle between two asteroids
+
+							if (input[y2][x2] == '#' and !(y2 == y and x2 == x))
 								angle_set.insert(atan2(y2 - y, x2 - x));
-							}
+
 						}
 					}
 					asteroid_count[x * 100 + y] = angle_set.size();
@@ -72,11 +69,8 @@ namespace Day10 {
 			return a.second < b.second;
 		});
 
-		int x = (max->first) / 100;
-		int y = (max->first) % 100;
+		int x = (max->first) / 100, y = (max->first) % 100;
 		std::cout << "Part 1 solution : " << max->second << " (at " << x << ", " << y << ")\n";
-
-
 
 
 		// Create angle map (map of angles on to vector of asteroids that fall on that angle)
@@ -113,7 +107,6 @@ namespace Day10 {
 					a.second.pop_back();
 				}
 			}
-
 		}
 
 		std::cout << "Part 2 solution : " << destroyed_asteroids[199] << "\n";
@@ -121,4 +114,3 @@ namespace Day10 {
 		std::cout << "\n";
 	}
 };
-
